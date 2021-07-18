31599da501f09fb4de6739afd1a214c7be198552
SergejViktorov <sergej.viktorov1310@gmail.com>
2021-07-18 17:02:48 +0300

    готово

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import React from 'react'
-import Feedback from './Components/Feedback'
+import { Component } from 'react'
 
-const App = () => {
-	return (
-		<>
-			<Feedback />
-		</>
-	)
+import Section from './Components/Section'
+import FeedbackOptions from './Components/FeedbackOptions'
+import Statistics from './Components/Statistics'
+import { FEEDBACK_OPTIONS } from './data/options'
+
+class App extends Component {
+	state = {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	}
+
+	leaveFeedback = ({ target }) => {
+		const { feedback } = target.dataset
+		this.setState((prevState) => ({ [feedback]: prevState[feedback] + 1 }))
+	}
+
+	countTotalFeedback = () => {
+		const { good, neutral, bad } = this.state
+		return good + neutral + bad
+	}
+
+	countPositive = () => {
+		const { good } = this.state
+		const total = this.countTotalFeedback()
+		return Math.trunc((good / total) * 100) || 0
+	}
+
+	render() {
+		const { good, neutral, bad } = this.state
+		const total = this.countTotalFeedback()
+		const positivePercentage = this.countPositive()
+		return (
+			<div>
+				<Section title="Please leave feedback">
+					<FeedbackOptions
+						options={FEEDBACK_OPTIONS}
+						onLeaveFeedback={this.leaveFeedback}
+					/>
+				</Section>
+
+				<Section title="Statistics">
+					<Statistics
+						good={good}
+						neutral={neutral}
+						bad={bad}
+						total={total}
+						positivePercentage={positivePercentage}
+					/>
+				</Section>
+			</div>
+		)
+	}
 }
 
 export default App
